c2497000b07525385ce48706413ecb9b434195a3
young40 <young40@gmail.com>
2025-01-23 13:36:21 +0800

    test

---
+++ Assets/Scripts/SkyboxChanger.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SkyboxChanger : MonoBehaviour
+{
+    [SerializeField] private Material[] skyboxes;
+
+    private void Start()
+    {
+        UpdateSkybox(0);
+        StartCoroutine(ChangeSkybox());
+    }
+
+    private IEnumerator ChangeSkybox()
+    {
+        var currentIndex = 0;
+        while (true)
+        {
+            yield return new WaitForSeconds(10f);
+            currentIndex++;
+            currentIndex %= skyboxes.Length;
+
+            UpdateSkybox(currentIndex);
+        }
+    }
+
+    private void UpdateSkybox(int index)
+    {
+        RenderSettings.skybox = skyboxes[index];
+    }
+
+    private void OnValidate()
+    {
+        if (skyboxes.Length != 0)
+        {
+            return;
+        }
+
+        var materials = AssetDatabase.FindAssets("t:Material");
+
+        List<Material> list = new List<Material>();
+
+        foreach (var material in materials)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(material);
+            if (path.Contains("Skybox"))
+            {
+                list.Add(AssetDatabase.LoadAssetAtPath<Material>(path));
+            }
+        }
+
+        skyboxes = list.ToArray();
+    }
+}
