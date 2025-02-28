8c8a1cbb9493bbb503e26ecf593e2db791a6b789
Bourne Young <young40@gmail.com>
2025-02-28 09:08:40 +0800

    test

---
+++ Assets/CopyColorFeature.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CopyColorFeature : ScriptableRendererFeature
+{
+    
+    
+    public override void Create()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
