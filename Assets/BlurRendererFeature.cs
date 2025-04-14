41f89673d7f628a79188400af5a7b22247b2e41c
Bourne Young <young40@gmail.com>
2025-04-14 15:25:32 +0800

    init

--- Assets/BlurRendererFeature.cs
+++ Assets/BlurRendererFeature.cs
@@ -1,13 +1,54 @@
+using System;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
+[Serializable]
+public class BlurSettings
+{
+    [Range(0.0f, 0.4f)] public float horizontalBlur;
+    [Range(0.0f, 0.4f)] public float verticalBlur;
+}
+
 public class BlurRendererFeature : ScriptableRendererFeature
 {
+    [SerializeField] private BlurSettings settings;
+    [SerializeField] private Shader shader;
+    [SerializeField] private Material material;
+    [SerializeField] private BlurRenderPass blurRenderPass;
+    
     public override void Create()
     {
+        if (shader == null)
+        {
+            return;
+        }
+        material = new Material(shader);
+        blurRenderPass = new BlurRenderPass(material, settings);
+
+        blurRenderPass.renderPassEvent = RenderPassEvent.AfterRenderingSkybox;
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
+        if (blurRenderPass == null)
+        { 
+            return;
+        }                
+        if (renderingData.cameraData.cameraType == CameraType.Game)
+        {
+            renderer.EnqueuePass(blurRenderPass);
+        }
     }
+    
+    protected override void Dispose(bool disposing)
+    {
+        if (Application.isPlaying)
+        {
+            Destroy(material);
+        }
+        else
+        {
+            DestroyImmediate(material);
+        }
+    } 
 }
